a684ba32463c266359870465151445d014868491
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2022-07-28 03:21:26 +0300

    first commit to vercel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import productImage from "./assets/product-image.png";
+import { useState, useEffect } from "react";
+
+import MockData from "./data/MockData";
 
 function App() {
+  const [productData, setProductData] = useState(MockData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <section className='section-wrapper'>
+          <div className='product-img'>
+            <img src={productImage} alt='product-img' />
+          </div>
+          <div className='product-wrapper'>
+            <div className='product-title'>
+              <h1>
+                Everyday items, we have something to suit every occassion.
+              </h1>
+              <p>
+                At suspendisse augue lectus arcu, accumsan ut sit poseuere vitae
+                sit tincidunt semper eu proin gravida cursus.
+              </p>
+              <a href='/#'>Shop All Everyday Items</a>
+            </div>
+
+            <div className='products'>
+              {productData.map((product) => {
+                return (
+                  <div className='product-card' key={product.id}>
+                    <img src={product.productImg} alt='' />
+                    <div className='product_description'>
+                      <p>{product.name}</p>
+                      <div className='product-price_color'>
+                        <span className='product_price'>{product.price}</span>
+                        <div className='swatches'>
+                          <span
+                            className='dot'
+                            style={{
+                              backgroundColor: product.avaliableColors[0],
+                            }}></span>
+                          <span
+                            className='dot'
+                            style={{
+                              backgroundColor: product.avaliableColors[1],
+                            }}></span>
+                          <span
+                            className='dot'
+                            style={{
+                              backgroundColor: product.avaliableColors[2],
+                            }}></span>
+                          <span
+                            className='dot'
+                            style={{
+                              backgroundColor: product.avaliableColors[3],
+                            }}></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
---
+++ src/data/MockData.js
@@ -0,0 +1,29 @@
+import product1img from "../assets/product1.png";
+import product2img from "../assets/product2.png";
+import product3img from "../assets/product3.png";
+
+const products = [
+  {
+    id: 1,
+    name: "Organic Skinny High Waist Raw Hem Jeans",
+    price: "€33.95",
+    avaliableColors: ["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"],
+    productImg: product1img,
+  },
+  {
+    id: 2,
+    name: "Organic Skinny High Waist Raw Hem Jeans",
+    price: "€33.95",
+    avaliableColors: ["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"],
+    productImg: product2img,
+  },
+  {
+    id: 3,
+    name: "Organic Skinny High Waist Raw Hem Jeans",
+    price: "€33.95",
+    avaliableColors: ["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"],
+    productImg: product3img,
+  },
+];
+
+export default products;
